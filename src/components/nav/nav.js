6da46c433735c961f743cc656cbf320cd0501593
uk090483 <konradullrich@me.com>
2020-07-09 16:14:52 +0200

    nav animation

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -6,35 +6,45 @@ import Section from '../container/section';
 import Slide from 'react-reveal/Slide';
 import Header from '../header/header';
 import Kreutz from "../../assets/Menu_Kreutz.svg";
+import Fade from 'react-reveal/Fade';
 
 import style from './nav.module.scss';
 
 export default function Nav() {
     const [open, setOpen] = useState(false);
 
+    console.log(open)
+
     return (
         <div>
-            <div style={{ width: 40, transform: 'translateX(10px) translateY(5px)' }} onClick={() => setOpen(!open)}><Burger></Burger></div>
+            <div style={{ width: 40, transform: 'translateX(10px) translateY(5px)', pointerEvents: 'all' }} onClick={() => setOpen(!open)}><Burger></Burger></div>
 
-            <Slide right when={open} duration={500}>
-                <div className={style.root} style={{ pointerEvents: open ? 'auto' : 'none' }}>
-                    <Header color='#F5C5D9'>
-                        <div></div> <a className={style.icon} style={{ width: 40 }} onClick={() => setOpen(!open)}><Kreutz></Kreutz></a>
-                    </Header>
-                    <Section backgroundColor='black' maxWidth={'1500px'}>
-                        <div className={style.linksRoot}>
-                            <Link activeClassName={style.active} to="/ausstellung/">AUSSTELLUNG</Link> <br />
-                            <Link activeClassName={style.active} to="/teilnehmen/">TEILNEHMEN</Link> <br />
-                            <Link activeClassName={style.active} to="/unterstützen/">UNTERSTÜTZEN</Link> <br />
-                            <Link activeClassName={style.active} to="/about/">WER IST FRIDA?</Link> <br />
-                            <Link activeClassName={style.active} to="/kontakt/">KONTAKT</Link> <br />
-                        </div>
-                    </Section>
+
+            <div className={`${style.root} ${open ? style.active : ''}`} style={{ pointerEvents: open ? 'auto' : 'none' }}>
+                <div className={style.aniCircle1} ></div>
+                <div className={style.aniCircle2} ></div>
+
+                <Header color='#F5C5D9'>
+                    <div></div> <a className={style.icon} style={{ width: 40 }} onClick={() => setOpen(!open)}><Kreutz></Kreutz></a>
+                </Header>
+
+                <Fade right cascade when={open}>
+                    <div className={style.linksRoot}>
+                        <h1> <Link activeClassName={style.active} to="/ausstellung/">AUSSTELLUNG</Link></h1>
+                        <h1> <Link activeClassName={style.active} to="/teilnehmen/">TEILNEHMEN</Link></h1>
+                        <h1> <Link activeClassName={style.active} to="/unterstützen/">UNTERSTÜTZEN</Link></h1>
+                        <h1> <Link activeClassName={style.active} to="/about/">WER IST FRIDA?</Link></h1>
+                        <h1> <Link activeClassName={style.active} to="/kontakt/">KONTAKT</Link></h1>
+                    </div>
+                </Fade>
+
+                <Fade bottom when={open} delay={500}>
                     <div style={{ position: "fixed", bottom: 0, width: '100%' }}>
                         <BigButton></BigButton>
                     </div>
-                </div>
-            </Slide>
+                </Fade>
+            </div>
+
         </div>
     )
 
