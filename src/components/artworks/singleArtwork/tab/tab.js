dfecbd8ff5f338e823dae63d1a24312f8b1d6747
uk090483 <konradullrich@me.com>
2020-08-30 11:52:59 +0200

    tab animation

--- src/components/artworks/singleArtwork/tab/tab.js
+++ src/components/artworks/singleArtwork/tab/tab.js
@@ -1,24 +1,44 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 import style from "./tab.module.scss"
 
 export default function Tab({ text1, text2 }) {
   const [active, setActive] = useState(true)
+  const [firstRender, setFirstRender] = useState(true)
+
+  const handleClick = () => {
+    if (firstRender) {
+      setFirstRender(false)
+    }
+    setActive(!active)
+  }
 
   return (
     <div className={style.root}>
       <div
         className={style.button}
         onClick={() => {
-          setActive(!active)
+          handleClick()
         }}
       >
         <div className={active ? style.active : ""}>Künstler Info</div>
         <div className={!active ? style.active : ""}>Kunstwerk Info</div>
       </div>
 
-      {active && <div className={style.text}>{text1}</div>}
-      {!active && <div className={style.text}>{text2}</div>}
+      {active && (
+        <div
+          className={`${style.text} ${!firstRender ? style.textActive : ""}`}
+        >
+          {text1}
+        </div>
+      )}
+      {!active && (
+        <div
+          className={`${style.text} ${!firstRender ? style.textActive : ""}`}
+        >
+          {text2}
+        </div>
+      )}
     </div>
   )
 }
