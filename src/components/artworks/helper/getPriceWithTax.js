3980f794698c3291457225611b3f1ee9f7b877dd
uk090483 <konradullrich@me.com>
2020-10-25 09:52:43 +0100

    tax fix

--- src/components/artworks/helper/getPriceWithTax.js
+++ src/components/artworks/helper/getPriceWithTax.js
@@ -3,11 +3,12 @@ export default function getPriceWithTax(price) {
     return ""
   }
 
-  const withTax = price * 1.16
+  // const withTax = price * 1.16
+  const withTax = (price / 84) * 100
 
   const roundTen = Math.ceil(withTax / 10) * 10
 
-  //   console.log(`Preis: ${price}/ mitMwSt: ${withTax}/ gerundet: ${roundTen}`)
+  // console.log(`Preis: ${price}/ mitMwSt: ${withTax}/ gerundet: ${roundTen}`)
 
   return roundTen
 }
