706bc32120f3b324fc86d5beebae0a27346f875d
uk090483 <konradullrich@me.com>
2020-12-13 18:54:57 +0100

    remove storyblok

--- src/storyblock/text.js
+++ src/storyblock/text.js
@@ -1,13 +1,14 @@
 import React, { useMemo } from "react"
-import RichTextResolver from "../../node_modules/storyblok-js-client/dist/rich-text-resolver.es"
-import SbEditable from "storyblok-react"
+// import RichTextResolver from "../../node_modules/storyblok-js-client/dist/rich-text-resolver.es"
+// import SbEditable from "storyblok-react"
 export default function Text(props) {
   const { text } = props.blok
-  const resolver = useMemo(() => new RichTextResolver(), [])
-  const html = resolver.render(text)
+  // const resolver = useMemo(() => new RichTextResolver(), [])
+  // const html = resolver.render(text)
   return (
-    <SbEditable content={props.blok}>
-      <div dangerouslySetInnerHTML={{ __html: html }} />
-    </SbEditable>
+    <div></div>
+    // <SbEditable content={props.blok}>
+    //   <div dangerouslySetInnerHTML={{ __html: html }} />
+    // </SbEditable>
   )
 }
